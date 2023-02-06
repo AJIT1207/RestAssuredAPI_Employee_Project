ff2768b66a0bba01a08ad3a0ad49a6ccf4f12e4a
ajitthefriend2009@gmail.com <ajit.jaiswal1309@gmail.com>
2023-02-06 13:57:03 +0530

    Initial commit

---
+++ src/main/java/RestAssuredAPI_Employee_Project/App.java
@@ -0,0 +1,13 @@
+package RestAssuredAPI_Employee_Project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
